a8fab79c59b452c500631a2820134287ed881722
gaozhao <gaozhao@optaim.com>
2018-10-11 08:54:19 +0800

    企业

--- routes/index.js
+++ routes/index.js
@@ -91,4 +91,89 @@ router.post('/getuserdata', function (req, res, next) {
     })
 });
 
+router.post('/getcompanyinfo', function (req, res, next) {
+    var companycode = req.body.companycode;
+
+    request({
+        url: 'http://220.248.87.150:8017/getcompanyinfo',
+        method: "POST",
+        headers: {
+            'API-VERSION': '1.1',
+            'USER-TOKEN': 'NryMrzKonYLE8KK1zSutUf4a2G32r1Gc',
+            'PLATFORM': 'IOS',
+            'DEVICE-ID': '1234234',
+            'APP-LEVEL-VERSION': '1.0.3',
+            'USER-MPHONE-BRAND': 'request',
+            'USER-MPHONE-MODELS': 'HAWEI',
+            'USER-MPHONE-OS-VER': '9.0',
+            "content-type": "application/json",
+            'User-Agent': 'request',
+            'CC': '86',
+            'KEY':'1GIL=223098ladj97100=-9dskja',
+            'PHONE': '18701766820'
+        },
+        body: JSON.stringify({
+            // userid:''
+            // toUserId: userid
+            companycode: companycode
+        }),
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            res.json({
+                code: 0,
+                ret_msg: JSON.parse(body)
+            });
+        }else{
+            res.json({
+                code:1,
+                ret_msg:error
+            })
+        }
+    })
+});
+
+router.post('/getproductinfo', function (req, res, next) {
+    var hscode = req.body.hscode;
+
+    request({
+        url: 'http://220.248.87.150:8017/getproductinfo',
+        method: "POST",
+        headers: {
+            'API-VERSION': '1.1',
+            'USER-TOKEN': 'NryMrzKonYLE8KK1zSutUf4a2G32r1Gc',
+            'PLATFORM': 'IOS',
+            'DEVICE-ID': '1234234',
+            'APP-LEVEL-VERSION': '1.0.3',
+            'USER-MPHONE-BRAND': 'request',
+            'USER-MPHONE-MODELS': 'HAWEI',
+            'USER-MPHONE-OS-VER': '9.0',
+            "content-type": "application/json",
+            'User-Agent': 'request',
+            'CC': '86',
+            'KEY':'1GIL=223098ladj97100=-9dskja',
+            'PHONE': '18701766820'
+        },
+        body: JSON.stringify({
+            // userid:''
+            // toUserId: userid
+            hscode: hscode
+        }),
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+            res.json({
+                code: 0,
+                ret_msg: JSON.parse(body)
+            });
+        }else{
+            res.json({
+                code:1,
+                ret_msg:error
+            })
+        }
+    })
+});
+
+
 module.exports = router;
