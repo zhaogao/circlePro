801f538c42f0fa1aaead072a8b661167553b9393
gaozhao <gaozhao@optaim.com>
2018-10-19 01:11:52 +0800

    修改

--- public/config.js
+++ public/config.js
@@ -1,3 +1,5 @@
 var config = {
     img_root:'http://120.79.19.33:8004/',
+    ios_app:'https://itunes.apple.com/cn/app/id1415575117?l=zh&ls=1&mt=8',
+    android_app:'https://www.circledin.net/download/app-release.apk',
 }
