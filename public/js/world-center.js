d54fdc1d7912971495e9b301881c35eedb6b7793
gaozhao <gaozhao@optaim.com>
2018-11-30 01:47:51 +0800

    修改

---
+++ public/js/world-center.js
@@ -0,0 +1,12 @@
+var data = [
+    {
+        name:'china',
+        name_chn:'中国',
+        center:[116.20,39.55] // 首都 经纬度
+    },
+    {
+        name:'china',
+        name_chn:'中国',
+        center:[116.20,39.55] // 首都 经纬度
+    },
+]
